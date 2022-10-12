5ffdd0e4373bf9de589ce7f38d83f3096a9e18bf
Sudharsanpandiarajan <mr.sudharsanofficial@gmail.com>
2022-10-12 17:18:06 +0530

    second update from A

--- src/org/demo/Employee.java
+++ src/org/demo/Employee.java
@@ -3,12 +3,10 @@ package org.demo;
 public class Employee {
 	
 	public static void main(String[] args) {
-		
-		
 		System.out.println(1);
-		
-		
-		
+		System.out.println("hello");
+		System.out.println("welcome");
+		System.out.println("Java class");
 	}
 
 }
